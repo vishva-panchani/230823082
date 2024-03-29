491a169a2317e20b227c313b30c1ad094974b7d3
vishva-panchani <vishvapanchani123@gmail.com>
2024-03-29 09:36:51 +0530

    Resume Template

---
+++ Resume/src/Resume.js
@@ -0,0 +1,190 @@
+ import profile from './images/profile.jpg';
+
+
+function Resume()
+ {
+    return (
+        <div>
+            <div className="container">
+                <div className="row" style={{"backgroundColor":"orange","height":"45px","width":"1200px"}}>
+                </div>
+                <div className="row ">
+                    <div className="col-md-8" style={{"marginRight": "30px"}}>
+                            <h1 style ={{"marginTop": "20px"}}>ISHAAN AGRAWAL</h1>
+                            <hr style ={{"backgroundColor":"#000000","height":"5px"}}></hr>
+                        <div className="row">
+                            <div className="col-4">
+                                <h5 ><b>PROFESSIONAL SUMMARY</b> </h5>
+                            </div>
+                            <div className="col-5">
+                                <hr style ={{"color":"orange","backgroundColor":"orange","width":"600px","height":"20px","marginLeft":"-35px","marginRight":"700px","marginBottom":"0px","marginTop":"0px"}}></hr>
+                            </div>
+                        </div>
+                        <div className="row">
+                                <p style={{"textAlign":"justify"}}>
+                                        I am an organised, efficient and hard working person, and am willing to discover and accept new 
+                                        ideas which can be put into practice effectively. I am a good listener and learner, able to c
+                                        ommunicate well with a group and on an individual level. I am able to motivate and direct my
+                                         talents and skills to meet objectives.
+                                </p>
+                        </div>
+                        <hr style ={{"backgroundColor":"black","height":"5px"}}></hr>
+                        <div className="row">
+                            <div className="col-2">
+                                 <h5 ><b>EXPRIENCE </b></h5>
+                            </div>
+                            <div className="col-10">
+                                 <hr style ={{"color":"orange","backgroundColor":"orange","width":"745px","height":"20px","marginLeft":"-35px","marginRight":"700px","marginBottom":"0px","marginTop":"0px"}}></hr>
+                            </div>
+                                <p>Retail Sales Associate, 02/2021 ,current</p>
+                                <p style={{"marginTop":"-20px","marginBottom":"0px"}}>ZARA-New Delhi,India</p>
+                                <ul style={{"marginLeft":"25px"}}>
+                                    <li>Begin with your years of experience and job title.
+                                         Start your summary by mentioning your years of professional and  experience  </li>       
+                                    <li>Begin with your years of experience and job title.
+                                         Start your summary by mentioning your years of professional and experience  </li>
+                                    <li>Begin with your years of experience and job title.
+                                         Start your summary by mentioning your years of professional and experience  </li>
+                                </ul>
+                                <br></br>
+                                <p>Retail Sales Associate, 02/2021 ,current</p>
+                                <p style={{"marginTop":"-20px","marginBottom":"0px"}}>ZARA-New Delhi,India</p>
+                                <ul style={{"marginLeft":"25px"}}>
+                                    <li>Begin with your years of experience and job title.
+                                         Start your summary by mentioning your years of professional and experience  </li>       
+                                    <li>Begin with your years of experience and job title.
+                                         Start your summary by mentioning your years of professional and experience  </li>
+                                    <li>Begin with your years of experience and job title.
+                                         Start your summary by mentioning your years of professional and experience  </li>
+                                    <li>Begin with your years of experience and job title.
+                                         Start your summary by mentioning your years of professional and experience  </li>
+                                </ul>
+                                <br></br>
+                                <p>Retail Sales Associate, 02/2021 ,current</p>
+                                <p style={{"marginTop":"-20px","marginBottom":"0px"}}>ZARA-New Delhi,India</p>
+                                <ul style={{"marginLeft":"25px"}}>
+                                    <li>Begin with your years of experience and job title.
+                                         Start your summary by mentioning your years of professional and experience  </li>       
+                                    <li>Begin with your years of experience and job title.
+                                         Start your summary by mentioning your years of professional and experience  </li>
+                                    <li>Begin with your years of experience and job title. </li>                                        
+                                    <li>Begin with your years of experience and job title.
+                                         Start your summary by mentioning your years of professional and experience  </li>
+                                    <li>Begin with your years of experience and job title.</li>                                                  
+                                    <li>Begin with your years of experience and job title.
+                                         Start your summary by mentioning your years of professional and experience  </li>
+                                    <li>Begin with your years of experience and job title.
+                                         Start your summary by mentioning your years of professional and experience  </li>
+                                </ul>
+                             </div>
+                             <br></br>
+                            <hr style ={{"backgroundColor":"black","height":"5px",}}></hr>
+                            <div className="row">
+                                 <div className="col-4">
+                                    <h5 ><b>EDUCATION</b> </h5>
+                                </div>
+                                 <div className="col-5">                                     
+                                     <hr style ={{"color":"orange","backgroundColor":"orange","width":"730px","height":"20px","marginLeft":"-170px","marginRight":"700px","marginBottom":"0px","marginTop":"0px"}}></hr>                                    
+                                </div>
+                                <p>Financial Accounting ,2018</p>
+                                 <p style={{"marginTop":"-20px","marginBottom":"0px"}}>Oxford Software Institute $ Oxford English School- New Delhi,</p>
+                                 <p style={{"marginTop":"-5px","marginBottom":"0px"}}>India</p>
+                        </div>   
+                    </div>
+
+                    <div className="col-md-4" style={{"marginRight":"-30px"}}>
+                        <div className="row">
+                            <img src={profile} alt="" style={{"height":"290px","width":"300px","marginTop":"40px"}}/>
+                        </div>
+                        <br></br>                       
+                        <div className="row">
+                                 <div className="col-3">
+                                    <h5 ><b>CONTACT</b> </h5>
+                                </div>
+                                 <div className="col-9">                                     
+                                     <hr style ={{"color":"orange","backgroundColor":"orange","width":"180px","height":"20px","marginLeft":"-15px","marginRight":"700px","marginBottom":"0px","marginTop":"0px"}}></hr>                                    
+                                </div>
+                                    <p>Address: New Delhi, India,200560</p>
+                                    <p style={{"marginTop":"-20px","marginBottom":"0px"}}>Phone: +9156456897123</p>
+                                    <p style={{"marginTop":"-5px","marginBottom":"0px"}}>Email:ishanagrawal@gmail.com</p>
+                                </div>
+                        <hr style ={{"backgroundColor":"black","height":"5px","width":"280px"}}></hr>                       
+                        <div className="row">
+                                 <div className="col-7">
+                                    <h5 ><b>CORE QUALIFICATION</b> </h5>
+                                </div>
+                                 <div className="col-5">                                     
+                                     <hr style ={{"color":"orange","backgroundColor":"orange","width":"70px","height":"20px","marginLeft":"-45px","marginRight":"700px","marginBottom":"0px","marginTop":"0px"}}></hr>                                    
+                                </div>
+                                <ul style={{"marginLeft":"20px"}}>
+                                    <li>Teamwork</li>
+                                    <li>Interpersonal communicati</li>
+                                    <li>Communication skills</li>
+                                    <li>Time management</li>
+                                    <li>Creativity</li>
+                                    <li>Problem-solving</li>
+                                    <li>Problem-solving</li>
+                                </ul>
+                            </div>
+
+                         <hr style ={{"backgroundColor":"black","height":"5px","width":"280px"}}></hr>
+                        <div className="row">
+                                 <div className="col-5">
+                                    <h5 ><b>LANGUAGES</b> </h5>
+                                </div>
+                                 <div className="col-7">                                     
+                                     <hr style ={{"color":"orange","backgroundColor":"orange","width":"150px","height":"20px","marginLeft":"-55px","marginRight":"700px","marginBottom":"0px","marginTop":"0px"}}></hr>                                    
+                                </div>
+                                <ul style={{"marginLeft":"20px"}}>
+                                    <li>Gujrati</li>
+                                    <li>Hindi</li>
+                                    <li>English</li>
+                                </ul>
+                         </div>
+
+                         <hr style ={{"backgroundColor":"black","height":"5px","width":"280px"}}></hr>
+                        <div className="row">
+                                 <div className="col-4">
+                                    <h5 ><b>INTEREST</b> </h5>
+                                </div>
+                                 <div className="col-8">                                     
+                                     <hr style ={{"color":"orange","backgroundColor":"orange","width":"185px","height":"20px","marginLeft":"-50px","marginRight":"700px","marginBottom":"0px","marginTop":"0px"}}></hr>                                    
+                                </div>
+                                <p style={{"marginTop":"-5px","marginBottom":"0px"}}>Recreation football league:</p>
+                                <ul style={{"marginLeft":"30px"}}>                                   
+                                    <li>Team Caption</li>
+                                    <li>Two-time league champion</li>
+                                </ul>
+                                <p style={{"marginTop":"-10px","marginBottom":"0px"}}>Local Community:</p>
+                                <ul style={{"marginLeft":"30px","textAlign":"justfy"}}>
+                                    <li>organive several performance</li>
+                                        
+                                </ul>
+                         </div>
+
+                         <hr style ={{"backgroundColor":"black","height":"5px","width":"280px"}}></hr>
+                        <div className="row">
+                                <h5 ><b>ADDITIONAL</b></h5> 
+                                 <div className="col-5">
+                                    <h5 ><b>INFORMATION</b></h5> 
+                                 
+                                </div>
+                                 <div className="col-7">                                     
+                                     <hr style ={{"color":"orange","backgroundColor":"orange","width":"140px","height":"20px","marginLeft":"-38px","marginRight":"700px","marginBottom":"0px","marginTop":"0px"}}></hr>                                    
+                                </div>
+                                <ul style={{"marginLeft":"20px"}}>
+                                    <li>Taught lifesaving skill and CPR <br/> classes</li>
+                                    <li>Red Cross Volunteer</li>
+                                   
+                                </ul>
+                         </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+  }
+  
+  export default Resume;
+  
